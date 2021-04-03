61b978cce0385ddb511723b286329851b607a8cf
reza amiri <reza.amiri.ac@gmail.com>
2021-04-03 10:15:37 +0430

    add user model

---
+++ app/model/account.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	"errors"
+	"regexp"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User is user model
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+// Validate validates fields of user model
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(
+			&u.Username,
+			validation.Required,
+			validation.Match(regexp.MustCompile("[a-zA-Z0-9]+")),
+		),
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required),
+	)
+}
+
+// HashPassword makes password field crypted
+func (u *User) HashPassword() error {
+	if len(u.Password) == 0 {
+		return errors.New("password should not be empty")
+	}
+	h, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(h)
+	return nil
+}
+
+// CheckPassword checks user password correct
+func (u *User) CheckPassword(plain string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plain))
+	return err == nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/rezaAmiri123/service-user
 
 go 1.14
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+	github.com/jinzhu/gorm v1.9.16
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
+)
